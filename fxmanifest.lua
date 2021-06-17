774d147e9db3e4ada107fbda3f949cb2e3676236
gubriansky@sernato.sk <gubriansky@sernato.sk>
2021-06-17 20:11:40 +0200

    moving sources around to give some structure

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'mufty'
+description 'MRP Races'
+version '0.0.1'
+
+server_scripts {
+    "shared/*.lua",
+    "server/*.lua"
+}
+
+client_scripts {
+    "shared/*.lua",
+    "client/*.lua"
+}
+
+dependency 'mrp_core'
